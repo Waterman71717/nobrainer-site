c1d4c40eb454c0b436a97e0c3929acaf39d497cf
Emmanuel Bakare <150776176+Waterman71717@users.noreply.github.com>
2025-06-27 00:44:25 -0500

    Integrate Google Tag Manager and remove direct HubSpot code from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -86,16 +86,27 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.className}>
       <head>
+        {/* Google Tag Manager (script part for head) */}
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KRDHFLTD');</script>
+        {/* End Google Tag Manager */}
+
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/favicon.ico" />
       </head>
       <body className="antialiased">
+        {/* Google Tag Manager (noscript part for body) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KRDHFLTD"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         {children}
 
-        {/* Start of HubSpot Embed Code */}
-        <script type="text/javascript" id="hs-script-loader" async defer src="//js-na2.hs-scripts.com/243105880.js"></script>
-        {/* End of HubSpot Embed Code */}
+        {/* The HubSpot tracking code has been removed from here */}
       </body>
     </html>
   )
